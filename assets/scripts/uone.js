9451f763e113221eed007fa88ed17a161d2f8094
MNicolso <71354582+MNicolso@users.noreply.github.com>
2020-09-30 22:41:15 -0700

    update

--- assets/scripts/uone.js
+++ assets/scripts/uone.js
@@ -173,150 +173,57 @@ function testimonialsCarousel() {
 }
 
 /*-------------------------
-          GOOGLE Map
+          RESUME/PROGRAMING
   -------------------------*/
-function mapInit() {
+// Get the modal
+var modal = document.getElementById("myModal");
 
-    "use strict";
-    if($('#my-map').length){
-        var options = {
-            center: new google.maps.LatLng(43.053454, -76.144508),
-            zoom: 11,
-            mapTypeControl: true,
-            panControl: false,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.DEFAULT,
-                position: google.maps.ControlPosition.TOP_LEFT
-            },
-            scaleControl: false,
-            styles: [{
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#576ee9"
-                }, {
-                    "lightness": 17
-                }]
-            }, {
-                "featureType": "landscape",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }, {
-                    "lightness": 20
-                }]
-            }, {
-                "featureType": "road.highway",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 17
-                }]
-            }, {
-                "featureType": "road.highway",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 29
-                }, {
-                    "weight": 0.2
-                }]
-            }, {
-                "featureType": "road.arterial",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 18
-                }]
-            }, {
-                "featureType": "road.local",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 18
-                }]
-            }, {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }, {
-                    "lightness": 21
-                }]
-            }, {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#d5d5d5"
-                }, {
-                    "lightness": 21
-                }]
-            }, {
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "visibility": "on"
-                }, {
-                    "color": "#f8f8f8"
-                }, {
-                    "lightness": 25
-                }]
-            }, {
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "saturation": 36
-                }, {
-                    "color": "#222222"
-                }, {
-                    "lightness": 30
-                }]
-            }, {
-                "elementType": "labels.icon",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            }, {
-                "featureType": "transit",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }, {
-                    "lightness": 19
-                }]
-            }, {
-                "featureType": "administrative",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#fefefe"
-                }, {
-                    "lightness": 10
-                }]
-            }, {
-                "featureType": "administrative",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#fefefe"
-                }, {
-                    "lightness": 17
-                }, {
-                    "weight": 1.2
-                }]
-            }],
-        };
-        var map = new google.maps.Map(document.getElementById('my-map'), options);
-        var marker1 = new google.maps.Marker({
-            position: map.getCenter(),
-            title: "Retrina",
-            icon: 'http://placehold.it/30x60',
-            animation: google.maps.Animation.BOUNCE
-        });
-        marker1.setMap(map);
-    }
+// Get the button that opens the modal
+var btn = document.getElementById("dotNet");
+var xamarin = document.getElementById("xamarin");
+
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";  
 }
+xamarin.onclick = function () {
+    modal.style.display = "block";
+    var parent = document.getElementById("parent");
+    var child = document.getElementById("child");
+    var para = document.createElement("p");
+    var node = document.createTextNode("xamarin text here.");
+    para.appendChild(node);
+    parent.replaceChild(para, child);
+}
+sql.onclick = function () {
+    modal.style.display = "block";
+    var parent = document.getElementById("parent");
+    var child = document.getElementById("child");
+    var para = document.createElement("p");
+    var node = document.createTextNode("sql text here.");
+    para.appendChild(node);
+    parent.replaceChild(para, child);
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 /*-------------------------
      AJAX CONTACT FORM
